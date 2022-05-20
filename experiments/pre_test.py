96253630b147a2404eda026b35fb2be36b9df687
CIG <dkesada@gmail.com>
2022-05-20 12:12:54 +0200

    testing bigger window

--- experiments/pre_test.py
+++ experiments/pre_test.py
@@ -2,16 +2,16 @@ from model.HFCM import HFCM
 import pandas as pd
 
 if __name__ == '__main__':
-    model = HFCM(optim='Nelder-Mead')
+    model = HFCM(optim='Nelder-Mead', window_size=10)
     # Prepro: normalization and cv
     dt = pd.read_csv("data/dt_cycles.csv")
     dt_test = dt.iloc[1100:1199, :]
     dt = dt.iloc[0:1000, :]  # 0:200 con 10000 its en el optim
     del dt['cyc']
     del dt_test['cyc']
-    # model.train_weights(dt)
-    model.load_model("HFCM_Wed_May_18_18_27_11_2022.json")
+    model.train_weights(dt)
+    # model.load_model("HFCM_Wed_May_18_18_27_11_2022.json")
     #model.forecast(dt_test, length=4, obj_vars=["T_1", "T_2"])
-    model.forecast(dt_test, length=4, obj_vars=["T_1"])
+    # model.forecast(dt_test, length=4, obj_vars=["T_1"])
     model.summarize()
 
